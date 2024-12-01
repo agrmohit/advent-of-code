6991ccf090923b5a4a9371d9b58cf847d8781ddb
Mohit Raj <mohit@agrmohit.com>
2024-12-02 00:34:25 +0530

    feat: day 01 part 1 solution

---
+++ 2024/01/main_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	_ "embed"
+	"testing"
+)
+
+//go:embed test1.txt
+var test1 string
+
+func TestSolution(t *testing.T) {
+	t.Run("Day 01 part 1 test 1", func(t *testing.T) {
+		want := 11
+		got := solvePart1(test1)
+
+		if got != want {
+			t.Errorf("Incorrect solution, got %d want %d", got, want)
+		}
+	})
+}
