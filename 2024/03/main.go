d2da5defe9a4c6fd261a7e8048abf6862be80be8
Mohit Raj <mohit@agrmohit.com>
2024-12-03 17:20:10 +0530

    feat: day 03 part 2 solution

--- 2024/03/main.go
+++ 2024/03/main.go
@@ -27,8 +27,37 @@ func solvePart1(input string) int {
 	return result
 }
 
+func solvePart2(input string) int {
+	rexp := regexp.MustCompile(`mul\((\d{1,3})\,(\d{1,3})\)|do\(\)|don't\(\)`)
+	matches := rexp.FindAllStringSubmatch(input, -1)
+
+	result := 0
+	enabled := true
+
+	for _, match := range matches {
+		switch match[0] {
+		case "do()":
+			enabled = true
+		case "don't()":
+			enabled = false
+		default:
+			// Ignore the error since regex already does the type matching
+			num1, _ := strconv.Atoi(match[1])
+			num2, _ := strconv.Atoi(match[2])
+
+			if enabled {
+				result += num1 * num2
+			}
+		}
+	}
+
+	return result
+}
+
 func main() {
 	part1Solution := solvePart1(input)
+	part2Solution := solvePart2(input)
 
 	fmt.Println("Day 03 Part 1 solution:", part1Solution)
+	fmt.Println("Day 03 Part 2 solution:", part2Solution)
 }
--- 2024/03/main_test.go
+++ 2024/03/main_test.go
@@ -8,10 +8,22 @@ import (
 //go:embed test1.txt
 var test1 string
 
+//go:embed test2.txt
+var test2 string
+
 func TestSolution(t *testing.T) {
 	t.Run("Day 03 part 1", func(t *testing.T) {
 		want := 161
 		got := solvePart1(test1)
+
+		if got != want {
+			t.Errorf("Incorrect solution, got %d want %d", got, want)
+		}
+	})
+
+	t.Run("Day 03 part 2", func(t *testing.T) {
+		want := 48
+		got := solvePart2(test2)
 
 		if got != want {
 			t.Errorf("Incorrect solution, got %d want %d", got, want)
